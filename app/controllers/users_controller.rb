ac01c2aaf5984812a5a2db7e65bb0533a92ae67b
Koya Yamamoto <koyayamamoto0@gmail.com>
2021-01-26 19:58:25 +0900

    create admin_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,5 +30,4 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
-
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,3 @@
 module ApplicationHelper
-  def flash_message
-      flash.each do |type, message|
-        content_tag(:div, message, class: "alert alert-#{type}")
-      end
-  end
+
 end
