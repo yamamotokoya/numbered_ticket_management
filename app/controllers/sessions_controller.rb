caf45a123c9e122bf1a17f6311a6c98b80498bb1
Koya Yamamoto <koyayamamoto0@gmail.com>
2021-02-01 12:34:41 +0900

    add remember

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       login user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to time_table_path, flash: {
         messages: {
           info: 'ログインしました'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   has_secure_password
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: true
@@ -41,5 +42,29 @@ class User < ApplicationRecord
 
   def self.paginate(array, page)
     Kaminari.paginate_array(array).page(page).per(15)
+  end
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_columns(remember_digest: User.digest(remember_token))
+  end
+
+  def forget
+    update_columns(remember_digest: nil)
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
