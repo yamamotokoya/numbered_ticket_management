561a80558ab09d9cc66933019285b256fffc2bde
Koya Yamamoto <koyayamamoto0@gmail.com>
2021-02-05 17:54:07 +0900

    fix

--- app/controllers/viewing_times_controller.rb
+++ app/controllers/viewing_times_controller.rb
@@ -2,6 +2,11 @@ class ViewingTimesController < ApplicationController
   before_action :redirect_to_login
 
   def show_time_table
-    @viewing_times = ViewingTime.get_today_schedule
+    @viewing_times = ViewingTime.get_today_schedule 
+    respond_to do |f|
+      f.html
+      f.json { @new_viewing_time = ViewingTime.where("id = ? and capacity < ?", params[:viewing_time][:id], 
+      params[:viewing_time][:capacity])}
+    end
   end
 end
